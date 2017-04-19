098ff684a5241fa719b530a8ae3a79a2322653e7
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-04-18 23:52:59 -0500

    Set app secret to env var

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(session({
-  secret: 'kelvinisthebest',
+  secret: process.env.APP_SECRET,
   resave: false,
   saveUninitialized: true,
 }));
